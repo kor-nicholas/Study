af8cf6d63a952d5c63a854247c06bb2cbc841dd4
Nicholas <kolya.com.org.ua@gmail.com>
2022-04-08 21:16:57 +0300

    Hotkey

---
+++ Hotkeys.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_CSharp_Design
+{
+    internal class Hotkeys
+    {
+        // Visual Studio
+
+        /*
+        * Ctrl + K AND Ctrl + D -> formating
+        * Ctrl + / OR Ctrl + Shift + / -> comment OR uncomment
+        * Ctrl + D OR (Ctrl + C AND Ctrl + V) -> copy string
+        * prop - propety
+        * Left ALT - squar oblast (write code in some strings one time)
+        * CTRL + Press - help (like ALT + Enter, but show what we can doing, not change)
+        * CTRL + M, CTRL + O - сворачивать all #regoins
+        * CTRL + M, CTRL + M - сворачивать определенное место, где стоит курсов
+        * 
+        * Конструктор - выделить все свойства -> создаьб конструктор
+        */
+
+        #region MyRegion
+
+        private void Method()
+        {
+            Console.WriteLine("");
+        }
+
+        #endregion
+    }
+}
