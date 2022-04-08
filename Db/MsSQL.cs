687f307fb2716657e584b58b9e61dcefe2cb9556
Nicholas <kolya.com.org.ua@gmail.com>
2022-04-08 21:12:55 +0300

    Db

---
+++ Db/MsSQL.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace HelloWorldWebAPI_Cat_.Study
+{
+    public class MsSQL
+    {
+        private static string ConnectionString { get; } = "Server=localhost;Database=cats;Trusted_Connection=True;";
+
+        // Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword; - STANDART
+        // Server=localhost;Database=cats;Trusted_Connection=True;
+
+        private static void SqlClose()
+        {
+            /*// Вариант 1 
+
+            SqlConnection connection = new SqlConnection(ConnectionString);
+
+            SqlCommand command = new SqlCommand("SELECT * FROM cats", connection);
+
+            var reader = command.ExecuteReader();
+
+            connection.Open();
+
+            reader.Close();
+            command.Clone();
+            connection.Close();
+
+            // ----------------------------------------------------------------------------------------------------------------------------------
+
+            // Вариант 2
+
+            using (SqlConnection connection2 = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command2 = new SqlCommand("SELECT * FROM cats", connection2))
+                {
+                    using (var reader2 = command2.ExecuteReader())
+                    {
+
+                    }
+                }
+            }*/
+        }
+
+        private void SqlExecute(SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand("SELECT TOP 100 * FROM cats", connection);
+
+            var reader = command.ExecuteReader(); // для select чтобы получить результат
+
+            if (reader.HasRows) // проверка что пришли строки
+            {
+                while (reader.Read()) // построчное чтение таблицы, пока есть строки - цикл работает 
+                {
+                    int someindex = 0;
+                    Console.WriteLine(reader[someindex]); // возможность работать как с массивом
+
+                    reader.GetBoolean(0); // взять bool значение из колонки 1
+                    reader.GetChar(1); // взять строку (либо 1 char, тут не знаю ??? ) из колонки 2  
+                    reader.GetDateTime(2); // взять дату из колонки 3  
+                    reader.GetValue(3); // достать какое-то значение из колонки 4
+
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    //DateTime datebirthday = new DateTime(reader.GetString(2));
+                    string dateBirthday = reader.GetString(2);
+                    string color = reader.GetString(3);
+
+                    Console.WriteLine($"id: {id} ; name: {name} ; datebirthday: {dateBirthday} ; color: {color}");
+                }
+            }
+        }
+
+        private static void SqlParameters(SqlConnection connection, string exampleStringParameter, int exampleIntParameter, DateTime examplaDateTimeParameter)
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM cats WHERE id < @limit", connection);
+
+            command.Parameters.AddWithValue("@limit", 10);// DECLARE @limit INT 10 ; потом его в sql-запросе можно просто юзать как переменную @limit
+            // Безопасная передача параметров от Sql-Injection (либо DECLARE либо через Parameters.Add / Parameters.AddWithValue)
+
+            char[] year = { '1', '9', '9', '1' };
+            command.Parameters.Add("@year", SqlDbType.Char, 4).Value = year;
+
+            command.Parameters.Add("@date", SqlDbType.DateTime).Value = examplaDateTimeParameter;
+
+            command.Parameters.Add("@limit", SqlDbType.Int).Value = exampleIntParameter;
+
+            command.Parameters.Add("@stringParameter", SqlDbType.Text).Value = exampleStringParameter;
+
+            var parameter = new SqlParameter("@limit", exampleIntParameter);
+            command.Parameters.Add(parameter);
+
+            parameter = new SqlParameter
+            {
+                ParameterName = "@limit",
+                Value = exampleIntParameter,
+                SqlDbType = SqlDbType.Int
+            };
+            command.Parameters.Add(parameter);
+
+
+        }
+
+        private static void SqlInsertUpdateDelete(SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand("INSERT INTO cats (id, name, dateBirthday, color) VALUES (1, 'Vasya', '01.01.1991', 'green')", connection);
+
+            command.ExecuteNonQuery(); // просто выполнить команду (для таких sql-запросов как INSERT, UPDATE, DELETE)
+        }
+    }
+}
+
---
+++ Db/Sql.cs
@@ -0,0 +1,62 @@
+﻿namespace HelloWorldWebAPI_Cat_.Study
+{
+    public class Sql
+    {
+        /*
+         * CREATE TABLE <table_name> (
+            name1 type,
+            name2 type,
+            PRIMARY KEY (name1) - первичный ключ (уникальное поле)
+            FOREIGN KEY (name1) REFERENCES table_name (name2) - внешний ключ
+            );
+
+            type :
+            INT - число
+            VARCHAR() - строка (максимум 255 символов), в скобках количество символов
+            TEXT - большой текст
+            DATE - формат для работы с датой
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY - для поля id
+            либо же в самом конце : PRIMARY KEY(id)
+
+            ALTER TABLE <table_name> ADD <name> <type> - добавить поле <name> в таблицу <table_name>
+            ALTER TABLE <table_name> CHANGE <name> <type> - изменить поле <name> в таблице <table_name>
+
+            DESCRIBE <table_name> - просмотр информации о столбцах таблицы (тип и так далее...)
+
+            INSERT INTO <table_name> - добавление данных в таблицу
+            INSERT INTO <table_name> (name1, name2, name3) VALUES (value1, value2, value3)
+            INSERT INTO <table_name> VALUES (value1, valu2, value3)
+
+            UPDATE <table_name> SET <name1> = <value1>, <name2> = <value2>, ... - обновить данные в таблице
+
+            DELETE FROM <table_name> WHERE <условие>- удаление данных из таблицы (DROP TABLE <table_name> - удаление целой таблицы)
+            TRUNCATE <table_name> - удаление всех полей в таблице
+
+            SELECT <name1> FROM <table_name> - получить данные из таблицы
+            SELECT * FROM <table_name> - взять все данные из таблицы
+            SELECT DISTINCT <name1> FROM <table_name> - получение неповторяющихся данных
+
+            WHERE - условие (так-же можно совмещать с AND, OR, NOT)
+            SELECT <name1> FROM <table_name> WHERE <условие>
+            SELECT * FROM users WHERE id>0
+
+            GROUP BY - сортировка данных
+            SELECT <name1>, <name2> FROM <table_name> DROUP BY <name2> - групировка по name2
+
+            HAVING - так же как WHERE, но более интересно
+            SELECT <name1>, <name2> FROM <table_name> GROUP BY <name2> HAVING COUNT либо MAX , ... <name1> > 1
+
+            ORDERED BY - сортировка по возрастанию / убыванию
+            SELECT <name1>, <name2> FROM <table_name> ORDERED BY <name1>, <name2> - по возрастанию
+            SELECT <name1>, <name2> FROM <table_name> ORDERED BY <name1>, <name2> DESC - по убыванию
+
+            BETWEEN - для выбора из промежутка
+            SELECT <name1>, <name2> FROM <table_name> WHERE <nameX> BETWEEN <value1> AND <value2>
+            Пример
+            Выведем список инструкторов, чья зарплата больше 50 000, но меньше 100 000:
+
+            SELECT * FROM instructor
+            WHERE salary BETWEEN 50000 AND 100000;
+         */
+    }
+}
