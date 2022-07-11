ede811c5bac018f163030972cf337d453f732d1f
Nicholas <65968471+kor-nicholas@users.noreply.github.com>
2022-07-11 12:49:24 +0300

    Create Sha256.cs

---
+++ C#/Encryption/Encryption/HashingAlgorithms/Sha256.cs
@@ -0,0 +1,10 @@
+public class HashPass(string password)
+{
+  using(var sha256 = SHA256.Create())
+  {
+    var hashBytes = sha256.ComputeHash(Encoding.UTF8.GerBytes(password));
+    var hash = BitConvert.ToString(hasheBytes).Replace("-", "").ToLower();
+    
+    return hash;
+  }
+}
