91123d76a99cd10b4da62c332ed9a29b75e49fcb
Nicholas <65968471+kor-nicholas@users.noreply.github.com>
2022-07-11 14:52:51 +0300

    Update Sha256.cs

--- C#/Encryption/Encryption/HashingAlgorithms/Sha256.cs
+++ C#/Encryption/Encryption/HashingAlgorithms/Sha256.cs
@@ -7,7 +7,7 @@ public class HashPass(string password)
   using(var sha256 = SHA256.Create())
   {
     var hashBytes = sha256.ComputeHash(Encoding.UTF8.GerBytes(password));
-    var hash = BitConvert.ToString(hasheBytes).Replace("-", "").ToLower();
+    var hash = BitConverter.ToString(hasheBytes).Replace("-", "").ToLower();
     
     return hash;
   }
