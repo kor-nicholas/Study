6a5860b7d898f8139de4f49dbf34e0f2de2dfb46
Nicholas <65968471+kor-nicholas@users.noreply.github.com>
2022-08-09 15:58:21 +0300

    Create DI.cs

---
+++ Bot/Deploy-f/DI.cs
@@ -0,0 +1,7 @@
+using Deployf.Botf;
+
+BotfProgram.StartBot(args, onConfigure: (svc, cfg) =>
+{
+  svc.AddTransient<ITestService, TestService>();
+  svc.AddSingleton<IBotUserService, UserService>();
+});
