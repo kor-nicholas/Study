98fe6e30aad8a00976336fa4d23323cb977d59b6
Korovchenko.Mykola <korovchenko.mykola@scheidt-bachmann.de>
2023-10-11 11:46:40 +0300

    Init project for book Thinking in Java

---
+++ ThinkingInJava/src/test/java/com/study/thinkinginjava/ThinkingInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.thinkinginjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkingInJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
